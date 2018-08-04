5a09a8037a4078adb06de469260d8e91e844f7e0
Nathaniel Kolenberg <nathanielkolenberg@Nathaniels-MacBook-Pro.local>
2018-08-04 15:22:29 +0400

    Add components to switch between alerts and use transition-group to animate multiple items

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@
                 <!-- Exercise 4 -->
                 <!-- Share the Computed Property rebuilding Exercise 2 via a Mixin -->
             </div>
+
             <div class="row">
                 <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                     <hr>
@@ -54,8 +55,8 @@
                     <br>
                     <br>
                     <transition 
-                        enter-active-class="animated bounce"
-                        leave-active-class="animated shake"
+                        enter-active-class="animated shake"
+                        leave-active-class="animated bounce"
                     > <!-- Transition allows you to animate anything that's between the opening and closing tag-->
                         <div class="alert alert-info" v-if="show">This comes using animated.css</div>
                     </transition>
@@ -71,13 +72,71 @@
                         <div class="alert alert-warning" v-else key="warning">This is some sliding warning through a warning key</div>
                     </transition>
                 </div>
+                <hr>
+                <br>
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button class="btn btn-info" @click="jsLoad = !jsLoad">
+                        Increase / Decrease Width through JS
+                    </button>
+                    <br>
+                    <br>
+                    <transition name="slide" 
+                        @before-enter="beforeEnter"
+                        @enter="enter"
+                        @after-enter="afterEnter"
+                        @enter-cancelled="enterCancelled"
+
+                        @before-leave="beforeLeave"
+                        @leave="leave"
+                        @after-leave="afterLeave"
+                        @leave-cancelled="leaveCancelled"
+                        :css="false"
+                        > <!-- mode="out-in" allows for the first element to be animated out before the new one gets animated in-->
+                        <div class="alert alert-info" v-if="jsLoad">This alert came through JavaScript</div>
+                    </transition>
+                </div>
+                <hr>
+                <hr>
+                <br>
+                <br>
+                <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                    <button class="btn btn-primary" @click="selectedComponent == 'app-success-alert' ? selectedComponent = 'app-danger-alert' : selectedComponent = 'app-success-alert' ">Toggle Component</button> 
+                    <br>
+                    <br>
+                    <transition name="fade" mode="out-in">
+                        <component :is="selectedComponent"></component>
+                    </transition>
+                    <hr>
+                    <br>
+
+                    <button class="btn btn-primary" @click="addItem">Add Item</button>
+                    <ul class="list-group">
+                    <!-- Transition group allows you to animate more than 1 item at the same time -->
+                        <transition-group name="slide">
+                            <!-- We need to provide a dynamic unique key to each li for Vue to animate them individually-->
+                            <li class="list-group-item"
+                                v-for="(number,item) in numbers" 
+                                @click="removeItem(item)"
+                                style="cursor: pointer;"
+                                :key="number"
+                                >
+                                {{number}}
+                            </li>
+                        </transition-group>
+                    </ul>
+                        
+                </div>
+                <hr>
+                <br>
             </div>
         </div>
     </div>
 </template>
 
 <script>
-import {computed} from "./computeProperties.js";
+import {computed} from "./computeProperties.js"
+import DangerAlert from "./DangerAlert.vue"
+import SuccessAlert from "./SuccessAlert.vue"
 
     export default {
         mixins: [computed],
@@ -87,7 +146,12 @@ import {computed} from "./computeProperties.js";
                 userInpCountEx: "",
                 show: true,
                 slide: false,
-                alertAnimation: ""
+                alertAnimation: "",
+                jsLoad: false,
+                elementWidth: 100,
+                selectedComponent: "app-success-alert",
+                numbers: [1, 2, 3, 4, 5, 6]
+
             }
         },
         filters: {
@@ -95,7 +159,70 @@ import {computed} from "./computeProperties.js";
                 return string.split("").reverse().join("");
             }
         },
-        
+        methods: {
+            addItem() {
+                let position = Math.floor(Math.random() * this.numbers.length);
+                this.key += 1;
+                this.numbers.splice(position, 0, this.numbers.length + 1);
+            },
+            removeItem(index) {
+                this.numbers.splice(index, 1);
+            },
+            beforeEnter(el) {
+                console.log("Before Enter");
+                this.elementWidth = 100;
+                el.style.width = this.elementWidth + "px";
+            },
+            enter(el, done) {
+                console.log("Enter");
+                let round = 1;
+                const interval = setInterval(()=>{
+                    el.style.width = (this.elementWidth + round * 10) + 'px';
+                    round++;
+                    if(round > 30) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20)
+            },
+            afterEnter(el) {
+                console.log("After Enter");
+                
+            },
+            enterCancelled(el) {
+                console.log("Enter Cancelled");
+            },
+
+
+            beforeLeave(el) {
+                console.log("Before Leave");
+                this.elementWidth = 400;
+                el.style.width = this.elementWidth + "px";
+
+            },
+            leave(el, done) {
+                console.log("Leave");
+                let round = 1;
+                const interval = setInterval(()=>{
+                    el.style.width = (this.elementWidth - round * 10) + 'px';
+                    round++;
+                    if(round > 20) {
+                        clearInterval(interval);
+                        done();
+                    }
+                }, 20)
+            },
+            afterLeave(el) {
+                console.log("After Leave");
+            },
+            leaveCancelled(el) {
+                console.log("Leave Cancelled");
+            }  
+        }, //End methods
+        components: {
+            'app-danger-alert': DangerAlert,
+            'app-success-alert': SuccessAlert
+        }
     }
 </script>
 
@@ -138,6 +265,11 @@ of the name property on the transition tag */
     animation: slide-out 1s ease-out forwards;
     transition: opacity 0.5s;
     opacity: 0;
+    position: absolute;
+}
+
+.slide-move {
+    transition: transform 1s;
 }
 
 @keyframes slide-in {
